7e33cebc0541f415f39cfd2a3469fee6585a343f
Salem Amba <salemamba@gmail.com>
2015-10-15 17:31:06 -0700

    Done coding by running test red/green

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,64 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return !arr.empty? ? arr.reduce(:+) : 0
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return !arr.empty? && arr.size >=2 ? sum(arr.sort!.pop(2)) : arr[0] || 0 
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).to_a.each{ |ar| return true if sum(ar) == n }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s=~/^[^aeiou\W]/i ? true : false 
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s=~/\A[\D]/ || s.empty?
+  s.to_i(2)%4==0  ? true : false
 end
 
-# Part 3
+
+# puts binary_multiple_of_4?('a100')
+#  puts binary_multiple_of_4?(' ')
+# puts binary_multiple_of_4?('100')
+# # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	attr_reader :price, :isbn 
+
+	def initialize(isbn="", price=0 )
+		self.isbn = isbn  
+		self.price = price.to_f 
+	end
+    
+    def isbn=(isbn)
+    	raise ArgumentError, "Invalide isbn number" if isbn.empty? 
+        @isbn = isbn
+    end
+
+    def price=(price)
+        raise ArgumentError, "Invalide price tag" if price <= 0 
+        @price = price 
+    end
+
+
+    def price_as_string
+		"$"<<price_formater
+	end
+
+	private
+		def price_formater
+			sprintf("%.2f", @price)
+		end
 end
